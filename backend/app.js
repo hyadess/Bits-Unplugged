fc65f9f0df174e4c6ae41bba6aa0ef1f736c8f07
mahirlabibdihan <mahirlabibdihan@gmail.com>
2023-11-11 12:49:14 +0600

    self ping added

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,31 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const cron = require("node-cron");
+var ping = require("ping");
+cron.schedule("*/12 * * * *", function () {
+  let host = "bitsunplugged.onrender.com";
+  var cfg = {
+    timeout: 100,
+    // WARNING: -i 2 may not work in other platform like windows
+    extra: ["-i", "2"],
+  };
+
+  ping.sys.probe(
+    host,
+    (isAlive) => {
+      var msg = isAlive
+        ? "host " + host + " is alive"
+        : "host " + host + " is dead";
+      console.log(msg);
+    },
+    cfg
+  );
+});
 // const fileUpload = require("express-fileupload");
 const appRoutes = require("./routes/appRoutes");
-// const corsOptions = {
-//   origin: "http://frontend", // Replace with your frontend's service name
-// };
-
-// app.use(cors(corsOptions));
 const CLIENT_BUILD_PATH = path.join(__dirname, "../frontend/build");
+
 
 app.use(cors());
 app.use(express.json());

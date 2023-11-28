804be4bcf33849e336fad4fb26e1720ed7804ccd
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2023-11-29 03:23:19 +0600

    ,

--- frontend/src/Components/Containers/CardContainer.jsx
+++ frontend/src/Components/Containers/CardContainer.jsx
@@ -30,7 +30,7 @@ import Zoom from "@mui/material/Zoom";
 
 const CardContainer = (props) => {
   return (
-    <div className="flex flex-row flex-wrap items-center justify-between items-center pb-8 mx-auto max-w-screen-2xl xl:gap-8 x:gap:16 gap-8 md:grid md:grid-cols-3">
+    <div className="flex flex-row flex-wrap items-center justify-center items-center pb-8 mx-auto max-w-screen-2xl xl:gap-8 x:gap:16 gap-8 md:grid md:grid-cols-3">
       {props.children}
     </div>
   );

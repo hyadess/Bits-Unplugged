3f78495ffcb0f929cafb221127da2164eb1f28d8
hyadess <sayemshahadsoummo@gmail.com>
2024-01-05 15:00:06 +0600

    inter mediate

--- backend/repository/submissionRepository.js
+++ backend/repository/submissionRepository.js
@@ -15,6 +15,30 @@ class SubmissionRepository extends Repository{
         const result = await this.query(query, params);
         return result;
     };
+    getAllSubmissionsByUser = async (user_id) => {
+        //console.log("lets see"+problem_id);
+        const query = `
+        SELECT * 
+        FROM Submissions S
+        WHERE S.user_id = $1;
+        `;
+        const params = [user_id];
+        const result = await this.query(query, params);
+        return result;
+    };
+    getAllSubmissionsByProblem = async (problem_id) => {
+        //console.log("lets see"+problem_id);
+        const query = `
+        SELECT * 
+        FROM Submissions S
+        WHERE S.problem_id = $1;
+        `;
+        const params = [problem_id];
+        const result = await this.query(query, params);
+        return result;
+    };
+
+
 
     submitSolution = async (user_id, problem_id,data) => {
         const query = `
@@ -26,6 +50,7 @@ class SubmissionRepository extends Repository{
         const result = await this.query(query, params);
         return result;
     };
+
 
 
 }

c601fa447a79f2f1e8fd5cff536ef1de0286b6be
mahirlabibdihan <mahirlabibdihan@gmail.com>
2024-01-09 03:27:53 +0600

    topic seed

--- backend/migrations/20240108211622-create-series.js
+++ backend/migrations/20240108211622-create-series.js
@@ -2,33 +2,34 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Series', {
+    await queryInterface.createTable("Series", {
+      // series?
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       topic_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       logo: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
--- backend/seeders/20240108194620-demo-topic.js
+++ backend/seeders/20240108194620-demo-topic.js
@@ -4,15 +4,39 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      "Topics",
+      "topics",
       [
         {
           id: 1,
+          name: "Graph",
+          description: "Nodes and Edges",
+          logo: "https://cdn0.iconfinder.com/data/icons/graph-4/100/graph1-512.png",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 2,
           name: "Recursion",
           description: null,
           logo: "https://i.pinimg.com/originals/a1/2b/a8/a12ba80f3701ef4e7014001e6c1869ac.gif",
-          createdAt: new Date(),
-          updatedAt: new Date(),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 3,
+          name: "Sorting",
+          description: null,
+          logo: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Sorting_shaker_sort_anim.gif",
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: 4,
+          name: "Tree",
+          description: null,
+          logo: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS7-pjys5IOotbaoxbBlxDklbg6YEPMwLcS0GOlOgRtEBWP_bQU",
+          created_at: new Date(),
+          updated_at: new Date(),
         },
       ],
       {}
@@ -20,6 +44,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Topics", null, {});
+    await queryInterface.bulkDelete("topics", null, {});
   },
 };

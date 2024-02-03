8179f9b09e6c53178a21048dac114280e1fae1c8
Mahir Labib Dihan <mahirlabibdihan@gmail.com>
2024-02-03 20:04:21 +0600

    Update CardContainer styling to include a max-width and adjust gap sizes

--- frontend/src/containers/CardContainer2.jsx
+++ frontend/src/containers/CardContainer2.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const CardContainer = (props) => {
   return (
     <div
-      className={`grid grid-cols-1 justify-center items-center mx-auto gap-8 md:grid-cols-${props.col} h-full w-full mb-3`}
+      className={`grid grid-cols-1 justify-center items-center mx-auto max-w-screen-2xl xl:gap-8 x:gap:8 gap-8 md:grid-cols-${props.col} h-full w-full mb-3`}
     >
       {props.children}
     </div>

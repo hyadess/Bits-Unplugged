03a0a0f844875796871c1f81f678d2168587de63
mahirlabibdihan <mahirlabibdihan@gmail.com>
2024-02-12 01:57:36 +0600

    practice icon

--- frontend/src/components/Navbars/SolverNavbar.jsx
+++ frontend/src/components/Navbars/SolverNavbar.jsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBrain,
   faChalkboardUser,
+  faHeadSideVirus,
   faHouse,
   faRightFromBracket,
 } from "@fortawesome/free-solid-svg-icons";
@@ -131,7 +132,7 @@ const SolverNavbar = (props) => {
               <NavButton
                 label={
                   <>
-                    <FontAwesomeIcon icon={faChalkboardUser} />
+                    <FontAwesomeIcon icon={faHeadSideVirus} />
                     Practice
                   </>
                 }

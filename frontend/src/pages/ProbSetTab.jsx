5a980048101364af34964a28e58d3a58c9ed2e4f
mahirlabibdihan <mahirlabibdihan@gmail.com>
2024-02-28 23:03:13 +0600

    tab left side

--- frontend/src/pages/ProbSetTab.jsx
+++ frontend/src/pages/ProbSetTab.jsx
@@ -4,13 +4,13 @@ const ProbSetTab = (props) => {
   // const tabs = ["Canvas", "Solution", "Test"];
 
   return (
-    <div className="flex w-full mx-auto justify-between mb-5 bu-card-primary rounded-full px-8">
-      <div className="p-0 flex flex-grow justify-between w-full gap-5">
+    <div className="fixed flex mx-auto justify-between mb-5 w-[20vw] px-8 left-0 top-1/2 transform -translate-y-1/2">
+      <div className="p-0 flex flex-col justify-between gap-5 w-full">
         {props.tabs.map((tab, index) => (
           <div
-            className={`cursor-pointer flex-grow px-5 py-2 text-center  font-bold text-2xl transition-colors duration-500  ${
+            className={`cursor-pointer flex-grow px-5 py-2 text-center font-bold text-2xl transition-colors duration-500  ${
               tab === props.activeTab
-                ? "border-b-4 bu-text-title"
+                ? "border-r-4 bu-text-title bg-slate-200"
                 : "bu-text-disable"
             }`}
             onClick={() => props.click(tab)}
